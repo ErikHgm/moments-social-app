61cce50448e1e7292b8a03a85aefb9b32f5ae1a6
Erik <erikhgm@gmail.com>
2022-11-06 16:59:02 +0000

    feat: add signup functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
+import { Switch, Route } from 'react-router-dom'
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Sign in</h1>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign in</h1>} />
+          <Route exact path='/signup' render={() => <SignUpForm />} />
+          <Route render={() => <h3>Page not found</h3>} />
+        </Switch>
       </Container>
     </div>
   );
