30c8a55049b8495b874f83df9d45295959c73edc
Erik <erikhgm@gmail.com>
2022-11-10 21:52:04 +0000

    feat: update navbar

---
+++ src/components/Avatar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../styles/Avatar.module.css'
+
+const Avatar = ({ src, heigth = 45, text }) => {
+    return <span>
+        <img className={styles.Avatar}
+            src={src}
+            height={heigth}
+            width={heigth}
+            alt='avatar' />
+            {text}
+    </span>
+};
+
+export default Avatar
