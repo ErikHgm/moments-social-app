200a12064736430db959e951264221aba4dc1e2c
Erik <erikhgm@gmail.com>
2022-11-11 22:48:49 +0000

    feat: create custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,11 +9,13 @@ import {
     useCurrentUser,
     useSetCurrentUser
 } from '../contexts/CurrentUserContext'
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle'
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
 
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -76,7 +78,7 @@ const NavBar = () => {
         </>
     )
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to='/'>
                     <Navbar.Brand>
@@ -84,7 +86,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)}
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'>
