aeeecdf6e70295f92a7c5138b9dd8a80a909bb5f
Erik <erikhgm@gmail.com>
2022-11-12 20:26:47 +0000

    feat: update postcreateform

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 import Asset from "../../components/Asset"
 import Upload from "../../assets/upload.png";
-import Image from "react-bootstrap/Image";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import { Alert } from "react-bootstrap";
 
 function PostCreateForm() {
 
@@ -24,6 +27,16 @@ function PostCreateForm() {
 
   const { title, content, image } = postData;
 
+  const imageInput = useRef(null);
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    })
+  }
+
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
@@ -32,15 +45,25 @@ function PostCreateForm() {
         image: URL.createObjectURL(event.target.files[0]),
       })
     }
+  };
 
-  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
 
-  const handleChange = (event) => {
-    setPostData({
-      ...postData,
-      [event.target.name]: event.target.value,
-    })
-  }
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", imageInput.current.files[0]);
+    try {
+      const { data } = await axiosReq.post("/posts/", formData);
+      history.push(`/posts/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
 
   const textFields = (
     <div className="text-center">
@@ -53,32 +76,44 @@ function PostCreateForm() {
           onChange={handleChange}
         >
         </Form.Control>
+        {errors?.title?.map((message, idx) => {
+          <Alert key={idx} variant="warning">
+            {message}
+          </Alert>
+        })
+        }
       </Form.Group> <Form.Group>
         <Form.Label>Content</Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
-          name="contect"
+          name="content"
           value={content}
           onChange={handleChange}
         >
         </Form.Control>
       </Form.Group>
+      {errors?.content?.map((message, idx) => {
+        <Alert key={idx} variant="warning">
+          {message}
+        </Alert>
+      })
+      }
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => { }}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
-      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit" >
         create
       </Button>
     </div>
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -110,8 +145,14 @@ function PostCreateForm() {
                 </>
               )
               }
-              <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} />
+              <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} ref={imageInput} />
             </Form.Group>
+            {errors?.image?.map((message, idx) => {
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            })
+            }
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
